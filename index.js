1fb73613ed323e35e1236a54f1dd9fb494cfb8c4
sandeepsinghvirdi <sandeep_sonu07@yahoo.com>
2022-06-12 22:44:25 +1000

    commit tree dd15ba3fb759d3de37b137aa6d44f6746f8c2285

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Your code here
+function saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+
+}
+
+function mondayWork(activity="go to the office"){
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(wrapper="*"){
+    return function (adjective="special"){
+        return `You are ${wrapper}${adjective}${wrapper}!`;
+    }
+}
+
+let Calculator = {
+    add: function(x,y){
+        return x + y;
+    },
+    subtract: function (x,y){
+        return x - y;
+    },
+    multiply: function (x,y){
+        return x * y;
+    },
+    divide: function (x,y){
+        return x / y;
+    }
+}
+
+function actionApplyer(startInteger, arrayOfFunctions){
+    if (arrayOfFunctions.length === 0){
+        return startInteger;
+    }
+    for (let i = 0; i < arrayOfFunctions.length; i++){
+        startInteger = arrayOfFunctions[i](startInteger);
+    }
+    return startInteger;
+}
